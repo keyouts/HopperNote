f8665c02f0fd8cb9a2048aa8596ed794dbb96a50
keyouts <admin@keyouts.com>
2026-06-02 20:01:43 -0400

    Add files via upload

---
+++ HopperNotev1.3.7/src/main/preload.js
@@ -0,0 +1,17 @@
+// Bridge Setup
+const { contextBridge, ipcRenderer } = require("electron")
+
+contextBridge.exposeInMainWorld("api", {
+  loadData() {
+    return ipcRenderer.invoke("load-data")
+  },
+  saveData(data) {
+    return ipcRenderer.invoke("save-data", data)
+  },
+  exportEntryHtml(payload) {
+    return ipcRenderer.invoke("export-entry-html", payload)
+  },
+  exportEntryPdf(payload) {
+    return ipcRenderer.invoke("export-entry-pdf", payload)
+  }
+})
